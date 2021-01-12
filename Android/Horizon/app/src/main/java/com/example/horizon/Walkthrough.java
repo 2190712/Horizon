2cf271f33a8ef33684df1b4e6b795fa49f6f27d8
2190712 <55537691+2190712@users.noreply.github.com>
2021-01-12 19:16:11 +0000

    update

--- Android/Horizon/app/src/main/java/com/example/horizon/Walkthrough.java
+++ Android/Horizon/app/src/main/java/com/example/horizon/Walkthrough.java
@@ -1,14 +1,22 @@
 package com.example.horizon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Walkthrough extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walkthrough);
+
+        mSlideViewPager = (ViewPager) findViewById(R.id.sliderViewPage);
+        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
     }
 }
